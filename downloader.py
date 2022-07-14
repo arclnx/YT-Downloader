c4e821d0f6806a0aace8bf40e8bda25ad39deb0f
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-07-14 10:05:32 -0700

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -8,6 +8,11 @@ def printInfo(yt):
     print("| " + title)
     print("+-----------------------------------+")
     print("Streams:")
-    print(yt.streams[0] , sep="\n")
+    
+    # Format stream into dictionary
+    streamDict = [{streamData[0]:streamData[1] for streamData in [
+        pair.split("=") for pair in str(stream)[1:-1].replace('"','').split(" ")[1:]]} for stream in streamList]
+    
+    print(*streamDict, sep="\n\n")
     
 printInfo(YouTube("https://www.youtube.com/watch?v=2lAe1cqCOXo"))
