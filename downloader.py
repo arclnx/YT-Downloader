f97a8a50161ffd19fc3114d02813d6aaa7acf49e
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-07-14 20:14:02 -0700

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -10,18 +10,18 @@ def printInfo(yt):
     print("Streams:")
     
     # Format stream into dictionary
-    streamDict = [{streamData[0]:streamData[1] for streamData in [
+    streamDictList = [{streamData[0]:streamData[1] for streamData in [
         pair.split("=") for pair in str(stream)[1:-1].replace('"','').split(" ")[1:]]} for stream in streamList]
     
-    return streamDict
+    return streamDictList
     
-def prettyPrintDict(dict, columnsToPrint):
+def prettyPrintDictList(dictList, columnsToPrint):
     
     # Find the maximum width of the value in the column or the column header itself
-    columnWidths = 
-    
+    columnWidths = [max([len(dictList[row].get(column,"")) for row in range(len(dictList))]) for column in columnsToPrint]
+
     return columnWidths
     
 print(*printInfo(YouTube("https://www.youtube.com/watch?v=2lAe1cqCOXo")), sep="\n\n")
 print("\n\n\n\n\n")
-print(prettyPrintDict(printInfo(YouTube("https://www.youtube.com/watch?v=2lAe1cqCOXo")), "itag"))
+print(prettyPrintDictList(printInfo(YouTube("https://www.youtube.com/watch?v=2lAe1cqCOXo")), ["itag","type"]))
