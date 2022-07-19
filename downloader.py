b51a837f97533b14a0c04dabb4c155550b4b34cb
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-07-19 14:58:01 -0700

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -20,15 +20,21 @@ def prettyPrintStreams(url, columnsToPrint):
     
     # Get YouTube video information
     yt = YouTube(url)
-    title=yt.title
+
     streamList = yt.streams
 
+    # Create a list of streams, each one a dict
     dictList = [{streamData[0]:streamData[1] for streamData in [
         pair.split("=") for pair in str(stream)[1:-1].replace('"','').split(" ")[1:]]} for stream in streamList]
     
     
     # Find the maximum width of the value in the column or the column header itself
     columnWidths = {column:max([len(dictList[row].get(column,"")) for row in range(len(dictList))]+[len(column)]) for column in columnsToPrint}
+    
+    # Print the video details
+    title = yt.title
+    author = yt.author
+    length = str(yt.seconds//3600).zfill(2) + ":" + str(yt.seconds%3600//60).zfill(2) + ":" + str(yt.seconds%3600%60)
     
     # Print the header of the table
     topHeader = "┌" + "┬".join(["─"*(columnWidths[column]+2) for column in columnsToPrint]) + "┐"
